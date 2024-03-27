bbc3c905b9d2e0d6f3c20839dadc0358ebb34cb8
simbr <brown.simon.a@gmail.com>
2024-03-27 22:50:02 +0100

    Basic admin crud

---
+++ src/Form/Type/AnimalTypeType.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AnimalTypeType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('label', TextType::class);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => \App\Entity\AnimalType::class
+        ]);
+    }
+}
