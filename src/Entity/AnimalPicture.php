e6472fa6bab75239c7550efe6b0e5079420d68aa
simbr <brown.simon.a@gmail.com>
2024-03-27 22:24:37 +0100

    Animal picture vich mapping

--- src/Entity/AnimalPicture.php
+++ src/Entity/AnimalPicture.php
@@ -4,9 +4,12 @@ namespace App\Entity;
 
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 #[ORM\Entity]
 #[ORM\Table(name: 'animal_picture')]
+#[Vich\Uploadable]
 class AnimalPicture
 {
     #[ORM\Id]
@@ -14,9 +17,26 @@ class AnimalPicture
     #[ORM\Column(name: 'id', type: Types::INTEGER)]
     private ?int $id = null;
 
-    #[ORM\ManyToOne(targetEntity: AnimalPicture::class, inversedBy: 'animal')]
+    #[ORM\ManyToOne(targetEntity: Animal::class, inversedBy: 'animal')]
     #[ORM\JoinColumn(name: 'animal_id', referencedColumnName: 'id', onDelete: 'CASCADE')]
     private ?Animal $animal = null;
+
+    #[Vich\UploadableField(mapping: 'animals', fileNameProperty: 'imageName', size: 'imageSize')]
+    private ?File $imageFile = null;
+
+    #[ORM\Column(name:'image_name', nullable: true)]
+    private ?string $imageName = null;
+
+    #[ORM\Column(name:'image_size', nullable: true)]
+    private ?int $imageSize = null;
+
+    #[ORM\Column(name:'updated_at', nullable: true)]
+    private ?\DateTimeImmutable $updatedAt = null;
+
+    public function __construct()
+    {
+        $this->updatedAt = new \DateTimeImmutable();
+    }
 
     public function getId(): ?int
     {
@@ -33,5 +53,47 @@ class AnimalPicture
         $this->animal = $animal;
 
         return $this;
+    }
+
+    public function setImageFile(?File $imageFile = null): self
+    {
+        $this->imageFile = $imageFile;
+
+        if (null !== $imageFile) {
+            // It is required that at least one field changes if you are using doctrine
+            // otherwise the event listeners won't be called and the file is lost
+            $this->updatedAt = new \DateTimeImmutable();
+        }
+
+        return $this;
+    }
+
+    public function getImageFile(): ?File
+    {
+        return $this->imageFile;
+    }
+
+    public function setImageName(?string $imageName): self
+    {
+        $this->imageName = $imageName;
+
+        return $this;
+    }
+
+    public function getImageName(): ?string
+    {
+        return $this->imageName;
+    }
+
+    public function setImageSize(?int $imageSize): self
+    {
+        $this->imageSize = $imageSize;
+
+        return $this;
+    }
+
+    public function getImageSize(): ?int
+    {
+        return $this->imageSize;
     }
 }
